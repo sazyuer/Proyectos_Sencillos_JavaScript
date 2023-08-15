691fb7062668176c15107c6829265a94f7b62cba
Josema <71278205+sazyuer@users.noreply.github.com>
2023-08-15 13:04:36 +0200

    correcion de espacios innecesarios

--- Generador de claves 2/index.js
+++ Generador de claves 2/index.js
@@ -7,44 +7,43 @@ let parrafo = document.querySelector("p");
 let posicionHijo;
 
 
+form.addEventListener("submit", (e) => {
+    if (!ComprobarNumero()) {
+        e.preventDefault();
 
-form.addEventListener("submit", (e)=>{
-   if(ComprobarNumero()!= true){
-    e.preventDefault();
-    
-   }else{
-    
-    crearContraseña();
-   }
+    } else {
+
+        crearContraseña();
+    }
 
 });
 
-largoContrasenya.addEventListener('blur',(e)=>{
-    
+largoContrasenya.addEventListener('blur', (e) => {
+
     let campo = e.target;
-    
-    if(parseInt(largoContrasenya.value) > 5 && parseInt(largoContrasenya.value) < 21){
+
+    if (parseInt(largoContrasenya.value) > 5 && parseInt(largoContrasenya.value) < 21) {
 
         let etiquetaSpan = document.querySelector("span");
-        if (etiquetaSpan){
+        if (etiquetaSpan) {
             etiquetaSpan.remove();
             campo.classList.remove("invalid");
         }
         //crearContraseña();       
-    }else{
-        
+    } else {
+
         crearEtiquetas(posicionHijo, parrafo, campo, "span", "error", "Error, debe ser un número entre 6 y 20");
         campo.classList.add("invalid")
-    }    
-} );
+    }
+});
 
 /*
 funcion para crear nuevas etiquetas html (posicionHijo)antes del cierre de la etiqueta html especificada(posicion). 
 Una vez creada una etiqueta html hay que añadirla al DOM y para ello esta la variable "posicionPadre". 
 Tambien añade el nombre que se quiera a la "class" y el texto que quieres que salga.
  */
-function crearEtiquetas(posicionHijo, posicionPadre, posicion, elementoHtml, clase, texto){
-        
+function crearEtiquetas(posicionHijo, posicionPadre, posicion, elementoHtml, clase, texto) {
+
     posicionHijo = document.createElement(elementoHtml);
     posicionPadre.appendChild(posicionHijo);
     posicion.nextElementSibling.classList.add(clase);
@@ -52,74 +51,65 @@ function crearEtiquetas(posicionHijo, posicionPadre, posicion, elementoHtml, cla
 }
 
 
-function eliminarExcesoParrafo (){
+function eliminarExcesoParrafo() {
 
     let borrarParrafo = document.getElementsByTagName("p");
 
+    if (borrarParrafo) {
+        for (let i = 2; i < borrarParrafo.length; i++) {
 
-    if (borrarParrafo){
-        for(let i= 2; i < borrarParrafo.length; i++){
-            
             borrarParrafo[i].remove();
         }
     }
 }
 
-function ComprobarNumero(){
+function ComprobarNumero() {
     let correcto = false;
-    
-    if(largoContrasenya.value > 5 && largoContrasenya.value < 21){
-         
+
+    if (largoContrasenya.value > 5 && largoContrasenya.value < 21) {
+
         correcto = true;
-    }else{
-       
-       
+    } else {
+
         correcto = false;
-        
+
     }
     return correcto;
-    
 }
 
 function crearContraseña() {
-    
-    
-        let longitud = largoContrasenya.value;
-        let clave = generarClave(longitud);
-        
-        crearEtiquetas(posicionHijo, parrafo,largoContrasenya , "p", "claveGenerada", clave);
-        
-        largoContrasenya.classList.remove("invalid");
-        ComprobarNumero(longitud);
-        eliminarExcesoParrafo();
-        
-    
-  }
-  
-  
-  /*
-    Función principal | Generador de claves | Password Generator
-  */
-  function generarClave(long)
-  {
-      /*caracteres permitidos*/
-      let caracteres = "%!Aa0BbCc1DdEe2FfGgHh3IiJj4KkLl5MmNn6OoPp7QqRr8SsTt9UuVv*WwXxYyZz$/?",
-          clave = '',
-          numeroClave;
-  
-      //creacion de clave
-      for(let i= 0; i < long; i++)
-      {
-           numeroClave = getNumero( 0, caracteres.length );
-           clave += caracteres.substring( numeroClave, numeroClave + 1 );
-      }
-      return clave;
-  }
-  
-  
-  //Función para generar un numero aleatorio
-  function getNumero(min,max)
-  {
-      return Math.floor( Math.random() * ( max - min ) ) + min;
-  }
+
+    let longitud = largoContrasenya.value;
+    let clave = generarClave(longitud);
+
+    crearEtiquetas(posicionHijo, parrafo, largoContrasenya, "p", "claveGenerada", clave);
+
+    largoContrasenya.classList.remove("invalid");
+    ComprobarNumero(longitud);
+    eliminarExcesoParrafo();
+
+}
+
+/*
+  Función principal | Generador de claves | Password Generator
+*/
+function generarClave(long) {
+    /*caracteres permitidos*/
+    let caracteres = "%!Aa0BbCc1DdEe2FfGgHh3IiJj4KkLl5MmNn6OoPp7QqRr8SsTt9UuVv*WwXxYyZz$/?",
+        clave = '',
+        numeroClave;
+
+    //creacion de clave
+    for (let i = 0; i < long; i++) {
+        numeroClave = getNumero(0, caracteres.length);
+        clave += caracteres.substring(numeroClave, numeroClave + 1);
+    }
+    return clave;
+}
+
+
+//Función para generar un numero aleatorio
+function getNumero(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
